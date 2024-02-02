1987ce8918b466930653e1219dd1a92fc6b21959
dani longo <resina00@gmail.com>
2024-02-02 18:46:25 +0100

    usimg new api ec2 url for api

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,3 @@
-// import { getAppStaticSettings } from "./store/static"
-
-// const staticSettings = getAppStaticSettings()
-
-
-// export const API_BASE = 'http://13.43.110.236:8080' 
-export const API_BASE = process.env.NODE_ENV === 'production' ? 'http://13.43.110.236:8080' : 'http://localhost:8080'
+export const API_BASE = process.env.NODE_ENV === 'production' ? 'https://api.qrated-review.net' : 'http://localhost:8080'
 
 export const GRAPHQL_ENDPOINT = `${ API_BASE }/graphql`
