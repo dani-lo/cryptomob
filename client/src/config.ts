8c94db79819b90e0531a07af250b7cfffb2669a2
dani longo <resina00@gmail.com>
2024-02-02 15:04:30 +0100

    changing api endpoint to new ec2 ip

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,9 @@
-import { getAppStaticSettings } from "./store/static"
+// import { getAppStaticSettings } from "./store/static"
 
-const staticSettings = getAppStaticSettings()
+// const staticSettings = getAppStaticSettings()
 
 
-export const API_BASE = process.env.NODE_ENV === 'production' ? staticSettings.url : 'http://localhost:8080'
+// export const API_BASE = 'http://13.43.110.236:8080' 
+export const API_BASE = process.env.NODE_ENV === 'production' ? 'http://13.43.110.236:8080' : 'http://localhost:8080'
+
 export const GRAPHQL_ENDPOINT = `${ API_BASE }/graphql`
